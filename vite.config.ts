87952293c6cabfbe54c3b3a97434947874406f4d
BigCorps <interpag.bigcorps@gmail.com>
2025-08-01 09:24:55 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig(({ mode }) => ({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
-        name: 'PoupeJá',
+        name: 'App Vixus',
         short_name: 'App Vixus',
         description: 'Gerencie suas finanças com IA',
         theme_color: '#005C6E',
