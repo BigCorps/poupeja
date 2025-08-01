a9a4835c15e84537b2fc35edc5b3eccec1f51c58
BigCorps <interpag.bigcorps@gmail.com>
2025-08-01 15:15:50 -0300

    Delete src/pages/DashboardSaldo.tsx

--- src/pages/DashboardSaldo.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SaldoDashboard from '../components/dashboard/SaldoDashboard';
-
-const DashboardSaldo = () => {
-  return <SaldoDashboard />;
-};
-
-export default DashboardSaldo;
