ebe3e0d6d81b7f0941a0863e1b5a0b999113823c
BigCorps <interpag.bigcorps@gmail.com>
2025-08-01 14:01:12 -0300

    Create DashboardSaldo.js

---
+++ src/pages/DashboardSaldo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SaldoDashboard from '../components/dashboard/SaldoDashboard';
+
+const DashboardSaldo = () => {
+  return <SaldoDashboard />;
+};
+
+export default DashboardSaldo;
