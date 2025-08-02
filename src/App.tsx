8253c69e9d7930f421cbea4ac72e594d6cec2270
BigCorps <interpag.bigcorps@gmail.com>
2025-08-02 10:08:02 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import ResetPasswordPage from "./pages/ResetPasswordPage";
 import ProfilePage from "./pages/ProfilePage";
 import TransactionsPage from "./pages/TransactionsPage";
+import SaldoDashboard from "./pages/SaldoDashboard";
 import ExpensesPage from "./pages/ExpensesPage";
 import GoalsPage from "./pages/GoalsPage";
 import ReportsPage from "./pages/ReportsPage";
@@ -30,9 +31,7 @@ import PaymentSuccessPage from "./pages/PaymentSuccessPage";
 import ThankYouPage from "./pages/ThankYouPage";
 import AdminDashboard from "./pages/AdminDashboard";
 import AchievementsPage from "./pages/AchievementsPage";
-import SaldoDashboard from "./pages/SaldoDashboard";
 import NotFound from "./pages/NotFound";
-import DashboardContent from "./pages/DashboardContent";
 import AdminRoute from "./components/admin/AdminRoute";
 import "./App.css";
 
@@ -51,40 +50,34 @@ function App() {
                     <BrowserRouter>
                       <Routes>
                         <Route path="/" element={<LandingPage />} />
+                        <Route path="/dashboard" element={<Index />} />
                         <Route path="/landing" element={<LandingPage />} />
                         <Route path="/login" element={<LoginPage />} />
                         <Route path="/register" element={<RegisterPage />} />
                         <Route path="/register/:planType" element={<RegisterWithPlanPage />} />
                         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
                         <Route path="/reset-password" element={<ResetPasswordPage />} />
+                        <Route path="/profile" element={<ProfilePage />} />
+                        <Route path="/transactions" element={<TransactionsPage />} />
+                        <Route path="/saldo" element={<SaldoDashboard />} />
+                        <Route path="/expenses" element={<ExpensesPage />} />
+                        <Route path="/goals" element={<GoalsPage />} />
+                        <Route path="/reports" element={<ReportsPage />} />
+                        <Route path="/schedule" element={<SchedulePage />} />
+                        <Route path="/settings" element={<SettingsPage />} />
+                        <Route path="/categories" element={<CategoriesPage />} />
+                        <Route path="/plans" element={<PlansPage />} />
                         <Route path="/checkout/:planType" element={<CheckoutPage />} />
                         <Route path="/payment-success" element={<PaymentSuccessPage />} />
                         <Route path="/thank-you" element={<ThankYouPage />} />
-                        
-                        {/* Rota principal do Dashboard com rotas aninhadas */}
-                        <Route path="/dashboard" element={<Index />}>
-                          {/* A rota "index" sem caminho renderiza a página do dashboard principal */}
-                          <Route index element={<DashboardContent />} />
-                          <Route path="saldo" element={<SaldoDashboard />} />
-                          <Route path="transactions" element={<TransactionsPage />} />
-                          <Route path="expenses" element={<ExpensesPage />} />
-                          <Route path="goals" element={<GoalsPage />} />
-                          <Route path="reports" element={<ReportsPage />} />
-                          <Route path="schedule" element={<SchedulePage />} />
-                          <Route path="settings" element={<SettingsPage />} />
-                          <Route path="categories" element={<CategoriesPage />} />
-                          <Route path="plans" element={<PlansPage />} />
-                          <Route path="profile" element={<ProfilePage />} />
-                          <Route path="achievements" element={<AchievementsPage />} />
-                        </Route>
-
-                        <Route
-                          path="/admin"
+                        <Route path="/achievements" element={<AchievementsPage />} />
+                        <Route 
+                          path="/admin" 
                           element={
                             <AdminRoute>
                               <AdminDashboard />
                             </AdminRoute>
-                          }
+                          } 
                         />
                         <Route path="*" element={<NotFound />} />
                       </Routes>
@@ -103,4 +96,3 @@ function App() {
 }
 
 export default App;
-
