33546e23b0a8e53640d7903dbd1aa69cf877de24
BigCorps <interpag.bigcorps@gmail.com>
2025-08-05 18:20:07 -0300

    Atualizar o AgenteIA.tsx

--- src/pages/AgenteIA.tsx
+++ src/pages/AgenteIA.tsx
@@ -17,7 +17,7 @@ const AgenteIA: React.FC = () => {
   useEffect(() => {
     // Detecta se é mobile e ajusta altura do iframe
     const isMobile = window.innerWidth <= 768;
-    const height = isMobile ? 'calc(110vh - 270px)' : 'calc(100vh - 100px)';
+    const height = isMobile ? 'calc(100vh - 230px)' : 'calc(100vh - 100px)';
     setIframeMinHeight(height);
   }, []);
 
