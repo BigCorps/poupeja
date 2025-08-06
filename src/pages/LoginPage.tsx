cc3944de31753fa897ae717cd3c707c60260e086
BigCorps <interpag.bigcorps@gmail.com>
2025-08-06 18:41:58 -0300

    Atualizar o LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
@@ -235,3 +234,4 @@ const LoginPage = () => {
 };
 
 export default LoginPage;
+
